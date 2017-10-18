87dfc819905e6538afa94838a2c089dada17fe5c
guigas <jimhorton7@outlook.com>
2017-10-18 11:56:42 -0200

    [simplicial] Implementa teste para função simplicial
    Signed-off-by: guigas <jimhorton7@outlook.com>

--- teste.c
+++ teste.c
@@ -7,18 +7,19 @@
 int main(void) {
 
   grafo g = le_grafo(stdin);
-  int d = diametro(g);
-
   if ( !g )
-
     return 1;
 
+  char *s = "Kyoko";
+  vertice v = vertice_nome(s, g); 
+  printf("%ssimplicial\n", simplicial(v, g) ? "" : "não ");
   printf("nome: %s\n", nome_grafo(g));
   printf("%d vértices\n", numero_vertices(g));
   printf("%d arestas\n", numero_arestas(g));
-  printf("%sdirecionado\n", direcionado(g) ? "" : "não ");
-  printf("%sbipartido\n", bipartido(g) ? "" : "não ");
+  printf("%sdirecionado\n", direcionado(g) ? "" : "não \n");
+  printf("%sbipartido\n", bipartido(g) ? "" : "não \n");
 
+  int d = diametro(g);
   if (d == infinito)
     printf("diâmetro = oo\n");
   else
