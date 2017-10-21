30674946d58662fdb2269721932a2a2600ff0bf1
guigas <jimhorton7@outlook.com>
2017-10-21 17:00:42 -0200

    [Simplicial] Arrumado bug com vertice_nome e passa teste do diâmetro pra frente
    Signed-off-by: guigas <jimhorton7@outlook.com>

--- teste.c
+++ teste.c
@@ -6,26 +6,29 @@
 
 int main(void) {
 
-  grafo g = le_grafo(stdin);
-  if ( !g )
-    return 1;
+	grafo g = le_grafo(stdin);
+	if ( !g )
+		return 1;
 
-  char *s = "Kyoko";
-  vertice v = vertice_nome(s, g); 
-  printf("%ssimplicial\n", simplicial(v, g) ? "" : "não ");
-  printf("nome: %s\n", nome_grafo(g));
-  printf("%d vértices\n", numero_vertices(g));
-  printf("%d arestas\n", numero_arestas(g));
-  printf("%sdirecionado\n", direcionado(g) ? "" : "não \n");
-  printf("%sbipartido\n", bipartido(g) ? "" : "não \n");
+	int d = diametro(g);
+	if (d == infinito)
+		printf("diâmetro = oo\n");
+	else
+		printf("diâmetro = %d\n", d);
 
-  int d = diametro(g);
-  if (d == infinito)
-    printf("diâmetro = oo\n");
-  else
-    printf("diâmetro = %d\n", d);
+	char *s = "A";
+	vertice v = vertice_nome(s, g); 
+	if (v != NULL) {
+		printf("%ssimplicial\n", simplicial(v, g) ? "" : "não ");
+	}
+	printf("nome: %s\n", nome_grafo(g));
+	printf("%d vértices\n", numero_vertices(g));
+	printf("%d arestas\n", numero_arestas(g));
+	printf("%sdirecionado\n", direcionado(g) ? "" : "não \n");
+	printf("%sbipartido\n", bipartido(g) ? "" : "não \n");
 
-  escreve_grafo(stdout, g);
 
-  return ! destroi_grafo(g);
+	escreve_grafo(stdout, g);
+
+	return ! destroi_grafo(g);
 }
